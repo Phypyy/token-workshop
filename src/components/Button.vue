09f7505c6ce6f0e24a114e93247e2e4ea612ad2b
Phypyy <supanut_kus@hotmail.com>
2023-04-19 17:47:01 +0700

    Setup

---
+++ src/components/Button.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <button class="button">{{ text }}</button>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'Button',
+    props: {
+      text: {
+        type: String,
+        required: true,
+        default: 'Button',
+      },
+    },
+  };
+  </script>
+  <style scoped>
+  .button {
+    background-color: var(--hris-primary-primary);
+    border: none;
+    border-radius: 8px;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+  .button :hover{
+    background-color: var(--hris-primary-hover);
+  }
+  .button :active{
+    background-color: var(--hris-primary-active);
+  }
+  </style> 
